11601f7cb75149a7a5cd3c2d298c7d6f3ad2dbca
Wofluently <f_liuchang@163.com>
2018-09-30 15:31:57 +0800

    启动Gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+// 导入compression-webpack-plugin
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// 定义压缩文件类型
+const productionGzipExtensions = ['js', 'css']
+
 module.exports = {
   // 配置反向代理
   devServer: {
@@ -12,5 +17,16 @@ module.exports = {
         }
       }
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
   }
 }
